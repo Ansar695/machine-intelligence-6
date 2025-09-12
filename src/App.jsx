0d5efbb68c2821d4ca0c40f9e930bc2033efccf0
ahsan798 <itsmeahsan98@gmail.com>
2025-09-12 16:24:43 +0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import About from "./pages/about";
+import Home from "./pages/home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
